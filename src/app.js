293c40ae6cbb9e908dcc58be1e2f6fb712175977
Danny Valdivia <dluis.valdivia@gmail.com>
2024-08-05 13:36:29 +0000

    domain name gen

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  let randomPronoun = Math.floor(Math.random() * pronoun.length);
+  let randomAdj = Math.floor(Math.random() * adj.length);
+  let randomNoun = Math.floor(Math.random() * noun.length);
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+
+console.log(randomPronoun);
 };
